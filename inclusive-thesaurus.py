d1d553218518401a16371a99bdc3b12c6365553d
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-04-18 21:54:12 -0700

    Update to use regex

--- inclusive-thesaurus.py
+++ inclusive-thesaurus.py
@@ -1,25 +1,38 @@
-import csv
 import os
-from nltk.tokenize import word_tokenize
+import csv
+import json
+import re
 from nltk import download
 
 # Download the NLTK data needed for tokenization
 download('punkt')
 
 def read_csv_file(file_path):
-    non_inclusive_terms = set()
+    non_inclusive_terms = {}
 
     with open(file_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
 
         for row in reader:
-            non_inclusive_terms.add(row['non_inclusive_term'].lower())
+            term = row['non_inclusive_term'].lower()
+            non_inclusive_terms[term] = {
+                'alternatives': row['inclusive_alternatives'],
+                'reason': row['note']
+            }
 
     return non_inclusive_terms
 
-def find_non_inclusive_terms(text, terms_set):
-    tokens = word_tokenize(text.lower())
-    return [token for token in tokens if token in terms_set]
+def find_non_inclusive_terms(text, terms_dict):
+    found_terms = []
+    added_terms = set()
+
+    for term in terms_dict:
+        pattern = r'\b' + re.escape(term) + r'\b'
+        if re.search(pattern, text, re.IGNORECASE) and term not in added_terms:
+            found_terms.append({'term': term, **terms_dict[term]})
+            added_terms.add(term)
+
+    return found_terms
 
 def main():
     csv_file_path = './inclusive-training-data.csv'
@@ -28,10 +41,8 @@ def main():
     input_text = os.environ.get('INPUT_TEXT', "Your default input text goes here.")
     found_terms = find_non_inclusive_terms(input_text, non_inclusive_terms)
 
-    if found_terms:
-        print("The following non-inclusive terms were found:", found_terms)
-    else:
-        print("No non-inclusive terms were found.")
+    result = {'non-inclusive-terms': found_terms}
+    print(json.dumps(result, indent=2))
 
 if __name__ == '__main__':
     main()
